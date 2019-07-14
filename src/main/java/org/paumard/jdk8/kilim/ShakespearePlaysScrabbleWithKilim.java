186dece1f8134e361e0c7210266ce16168a05aa1
lytles@takashi <srl.source@nqzero.com>
2019-07-14 16:26:26 -0400

    add disruptor and jctools implementations

--- src/main/java/org/paumard/jdk8/kilim/ShakespearePlaysScrabbleWithKilim.java
+++ src/main/java/org/paumard/jdk8/kilim/ShakespearePlaysScrabbleWithKilim.java
@@ -17,14 +17,19 @@
 
 package org.paumard.jdk8.kilim;
 
+import com.conversantmedia.util.concurrent.DisruptorBlockingQueue;
+import com.conversantmedia.util.concurrent.SpinPolicy;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import kilim.ForkJoinScheduler;
 import kilim.Mailbox;
 import kilim.Pausable;
 import kilim.Scheduler;
 import kilim.Task;
+import org.jctools.queues.SpmcArrayQueue;
+import org.jctools.queues.SpscArrayQueue;
 
 import org.openjdk.jmh.annotations.*;
 import org.paumard.jdk8.bench.ShakespearePlaysScrabble;
@@ -46,12 +51,24 @@ import org.paumard.jdk8.bench.ShakespearePlaysScrabble;
  * @author akarnokd
  * @author nqzero
  */
-public class ShakespearePlaysScrabbleWithKilim extends ShakespearePlaysScrabble {
-    static int numPool = Math.max(1, Scheduler.defaultNumberThreads - 1);
-    static {
-        Scheduler.setDefaultScheduler(new ForkJoinScheduler(numPool));
-    }
+@Fork(1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations=5, time=1)
+@Measurement(iterations=5, time=1)
+public abstract class ShakespearePlaysScrabbleWithKilim extends ShakespearePlaysScrabble {
+    static int numPool = Math.max(1, Scheduler.defaultNumberThreads-1);
+    static int size = 1<<10;
 
+    interface Jmh {
+        public List<Entry<Integer, List<String>>> measureThroughput() throws InterruptedException;
+    }
+    public static abstract class Base extends ShakespearePlaysScrabbleWithKilim implements Jmh {
+        void doMain() throws Exception {
+            init();
+            System.out.println(measureThroughput());
+        }
+    }
 
     static class Count {
         int num;
@@ -65,50 +82,163 @@ public class ShakespearePlaysScrabbleWithKilim extends ShakespearePlaysScrabble 
     }
     String stop = new String();
 
-    class Worker extends Task<ArrayList<Count>> {
-        int size = 1<<8;
-        Mailbox<String> box = new Mailbox(size,size);
+    public static class Threaded extends Base {
+        SpmcArrayQueue<String> queue;
+        @Benchmark
+        public List<Entry<Integer, List<String>>> measureThroughput() throws InterruptedException {
+            queue = new SpmcArrayQueue(size);
+            Runner [] actors = new Runner[numPool];
+            for (int ii=0; ii < actors.length; ii++)
+                (actors[ii] = new Runner()).start();
+            for (String word : shakespeareWords)
+                while (! queue.offer(word));
+            for (int ii=0; ii < actors.length; ii++)
+                while (! queue.offer(stop));
 
-        public void execute() throws Pausable {
-            ArrayList<Count> list = new ArrayList<>();
-            for (String word; (word = box.get()) != stop;) {
-                Integer num = getWord(word);
-                if (num != null)
-                    list.add(new Count(num,word));
+            TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
+            for (Runner actor : actors) {
+                actor.join();
+                for (Count count : actor.list)
+                    addWord(count.num, count.word, treemap);
             }
-            exit(list);
+            return getList(treemap);
+        }
+        class Runner extends Thread {
+            ArrayList<Count> list = new ArrayList<>();
+            public void run() {
+                for (String word; (word = queue.poll()) != stop;) {
+                    Integer num = getWord(word);
+                    if (num != null)
+                        list.add(new Count(num,word));
+                }
+            }
+        }
+    }
+
+    public static class Flat extends Base {
+        @Benchmark
+        public List<Entry<Integer, List<String>>> measureThroughput() throws InterruptedException {
+            Runner [] actors = new Runner[numPool];
+            for (int ii=0; ii < actors.length; ii++)
+                (actors[ii] = new Runner()).start();
+            int target = 0;
+            for (String word : shakespeareWords) {
+                if (++target==numPool) target = 0;
+                while (!actors[target].queue.offer(word));
+            }
+            for (int ii=0; ii < actors.length; ii++)
+                while (! actors[ii].queue.offer(stop));
+
+            TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
+            for (Runner actor : actors) {
+                actor.join();
+                for (Count count : actor.list)
+                    addWord(count.num, count.word, treemap);
+            }
+            return getList(treemap);
+        }
+        class Runner extends Thread {
+            SpscArrayQueue<String> queue = new SpscArrayQueue(size);
+            ArrayList<Count> list = new ArrayList<>();
+            public void run() {
+                for (String word; (word = queue.poll()) != stop;) {
+                    Integer num = getWord(word);
+                    if (num != null)
+                        list.add(new Count(num,word));
+                }
+            }
         }
     }
     
-    @SuppressWarnings("unused")
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime)
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Warmup(
-        iterations=20
-    )
-    @Measurement(
-        iterations=20
-    )
-    @Fork(5)
-    public List<Entry<Integer, List<String>>> measureThroughput() {
-        Worker [] actors = new Worker[numPool];
-        int target = 0;
-        for (int ii=0; ii < actors.length; ii++)
-            (actors[ii] = new Worker()).start();
-        for (String word : shakespeareWords) {
-            if (target==numPool) target = 0;
-            actors[target++].box.putb(word);
-        }
-        for (Worker actor : actors)
-            actor.box.putb(stop);
+    public static class Conversant extends Base {
+        @Benchmark
+        public List<Entry<Integer, List<String>>> measureThroughput() throws InterruptedException {
+            Runner [] actors = new Runner[numPool];
+            for (int ii=0; ii < actors.length; ii++)
+                (actors[ii] = new Runner()).start();
+            int target = 0;
+            for (String word : shakespeareWords) {
+                if (++target==numPool) target = 0;
+                actors[target].queue.put(word);
+            }
+            for (int ii=0; ii < actors.length; ii++)
+                actors[ii].queue.put(stop);
 
-        TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
-        for (Worker actor : actors)
-            for (Count count : actor.joinb().result)
-                addWord(count.num, count.word, treemap);
-        return getList(treemap);
+            TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
+            for (Runner actor : actors) {
+                actor.join();
+                for (Count count : actor.list)
+                    addWord(count.num, count.word, treemap);
+            }
+            return getList(treemap);
+        }
+        class Runner extends Thread {
+            private DisruptorBlockingQueue<String> queue =
+                    new DisruptorBlockingQueue<>(size, SpinPolicy.SPINNING);
+            ArrayList<Count> list = new ArrayList<>();
+            public void run() {
+                for (String word; (word = queue.poll()) != stop;) {
+                    Integer num = getWord(word);
+                    if (num != null)
+                        list.add(new Count(num,word));
+                }
+            }
+        }
     }
+    
+    
+    public static class Direct extends Base {
+        @Benchmark
+        public List<Entry<Integer, List<String>>> measureThroughput() {
+            TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
+            for (String word : shakespeareWords) {
+                Integer num = getWord(word);
+                if (num != null)
+                    addWord(num, word, treemap);
+            }
+            return getList(treemap);
+        }
+    }
+
+    public static class Kilim extends Base {
+        static {
+            Scheduler.setDefaultScheduler(new ForkJoinScheduler(numPool));
+        }
+        @Benchmark
+        public List<Entry<Integer, List<String>>> measureThroughput() throws InterruptedException {
+            Worker [] actors = new Worker[numPool];
+            int target = 0;
+            for (int ii=0; ii < actors.length; ii++)
+                (actors[ii] = new Worker()).start();
+            for (String word : shakespeareWords) {
+                if (target==numPool) target = 0;
+                actors[target++].box.putb(word);
+            }
+            for (Worker actor : actors)
+                actor.box.putb(stop);
+
+            TreeMap<Integer, List<String>> treemap = new TreeMap<Integer, List<String>>(Comparator.reverseOrder());
+            for (Worker actor : actors)
+                for (Count count : actor.joinb().result)
+                    addWord(count.num, count.word, treemap);
+            return getList(treemap);
+        }
+
+        class Worker extends Task<ArrayList<Count>> {
+            Mailbox<String> box = new Mailbox(size,size);
+
+            public void execute() throws Pausable {
+                ArrayList<Count> list = new ArrayList<>();
+                for (String word; (word = box.get()) != stop;) {
+                    Integer num = getWord(word);
+                    if (num != null)
+                        list.add(new Count(num,word));
+                }
+                exit(list);
+            }
+        }
+    }
+
     Integer getWord(String word) {
             if (scrabbleWords.contains(word)) {
                 HashMap<Integer, MutableLong> wordHistogram = new LinkedHashMap<>();
@@ -195,9 +325,11 @@ public class ShakespearePlaysScrabbleWithKilim extends ShakespearePlaysScrabble 
     }
 
     public static void main(String[] args) throws Exception {
-        ShakespearePlaysScrabbleWithKilim s = new ShakespearePlaysScrabbleWithKilim();
-        s.init();
-        System.out.println(s.measureThroughput());
+        new Threaded().doMain();
+        new Flat().doMain();
+        new Conversant().doMain();
+        new Kilim().doMain();
+        new Direct().doMain();
     }
 
     static class MutableLong {
@@ -222,3 +354,38 @@ public class ShakespearePlaysScrabbleWithKilim extends ShakespearePlaysScrabble 
         }
     }
 }
+
+/*
+
+num-threads is much than -1 for all, and much slower for Conv and Flat
+
+java 8, 8 bits
+ShakespearePlaysScrabbleWithKilim.Conversant.measureThroughput  avgt    5  4.671 ± 0.983  ms/op
+ShakespearePlaysScrabbleWithKilim.Direct.measureThroughput      avgt    5  7.164 ± 0.977  ms/op
+ShakespearePlaysScrabbleWithKilim.Flat.measureThroughput        avgt    5  4.661 ± 0.971  ms/op
+ShakespearePlaysScrabbleWithKilim.Kilim.measureThroughput       avgt    5  4.065 ± 0.236  ms/op
+ShakespearePlaysScrabbleWithKilim.Threaded.measureThroughput    avgt    5  4.919 ± 0.726  ms/op
+
+java 12, 8 bits
+ShakespearePlaysScrabbleWithKilim.Conversant.measureThroughput  avgt    5  4.977 ± 0.777  ms/op
+ShakespearePlaysScrabbleWithKilim.Direct.measureThroughput      avgt    5  7.453 ± 0.106  ms/op
+ShakespearePlaysScrabbleWithKilim.Flat.measureThroughput        avgt    5  5.227 ± 0.853  ms/op
+ShakespearePlaysScrabbleWithKilim.Kilim.measureThroughput       avgt    5  4.257 ± 0.383  ms/op
+ShakespearePlaysScrabbleWithKilim.Threaded.measureThroughput    avgt    5  4.709 ± 0.758  ms/op
+
+java 12, 10 bits
+ShakespearePlaysScrabbleWithKilim.Conversant.measureThroughput  avgt    5  5.010 ± 0.197  ms/op
+ShakespearePlaysScrabbleWithKilim.Direct.measureThroughput      avgt    5  7.443 ± 0.171  ms/op
+ShakespearePlaysScrabbleWithKilim.Flat.measureThroughput        avgt    5  5.129 ± 1.033  ms/op
+ShakespearePlaysScrabbleWithKilim.Kilim.measureThroughput       avgt    5  3.965 ± 0.155  ms/op
+ShakespearePlaysScrabbleWithKilim.Threaded.measureThroughput    avgt    5  4.768 ± 0.861  ms/op
+
+java 12, 10 bits, without jdk10 classifier
+ShakespearePlaysScrabbleWithKilim.Conversant.measureThroughput  avgt    5  5.249 ± 0.451  ms/op
+ShakespearePlaysScrabbleWithKilim.Direct.measureThroughput      avgt    5  7.544 ± 0.248  ms/op
+ShakespearePlaysScrabbleWithKilim.Flat.measureThroughput        avgt    5  5.202 ± 0.611  ms/op
+ShakespearePlaysScrabbleWithKilim.Kilim.measureThroughput       avgt    5  4.029 ± 0.203  ms/op
+ShakespearePlaysScrabbleWithKilim.Threaded.measureThroughput    avgt    5  4.729 ± 0.761  ms/op
+
+
+*/
